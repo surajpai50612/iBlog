d3aefb56dca02f8e367a5f4d977a0b0c93826896
surajpai50612 <surajpai50612@gmail.com>
2022-04-27 18:58:15 +0530

    third commit (setup- /posts)

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+
   devise_for :authors
   get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   root to: "home#index"
+
+  scope module: 'authors' do
+    resources :posts
+  end
   
   # Defines the root path route ("/")
   # root "articles#index"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_27_130944) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_27_131746) do
   create_table "authors", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -23,4 +23,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_27_130944) do
     t.index ["reset_password_token"], name: "index_authors_on_reset_password_token", unique: true
   end
 
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.boolean "published"
+    t.datetime "published_at"
+    t.integer "author_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_posts_on_author_id"
+  end
+
+  add_foreign_key "posts", "authors"
 end
