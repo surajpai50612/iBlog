0694d91aea407f7eb2cce63957e0176143670459
surajpai50612 <surajpai50612@gmail.com>
2022-05-01 11:17:01 +0530

    seventh commit (setup- rich_text_area)

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,24 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import "trix"
+import "@rails/actiontext"
+
+document.addEventListener('turbo:load', () => {
+    document.addEventListener('click', () => {
+        let element = event.target.closest('.paragraph-content')
+        if(!element) return;
+
+        element.classList.add('d-none')
+        element.nextElementSibling.classList.remove('d-none')
+    })
+
+    document.addEventListener('click', () => {
+        if(!event.target.matches('.cancel')) return;
+        
+        let element = event.target.closest('.paragraph-form')
+
+        element.classList.add('d-none')
+        element.previousElementSibling.classList.remove('d-none')
+    })
+})
