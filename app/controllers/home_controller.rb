92790e503c7a59f3d6ac7bda31e9167488fab9d0
surajpai50612 <surajpai50612@gmail.com>
2022-05-10 13:32:04 +0530

    fifteenth commit (completed- search)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
 class HomeController < ApplicationController
   def index
-  end
+    para = params[:key]
+
+    if para != nil
+      @allPosts = Post.where("title LIKE ?","%"+para+"%").order(created_at: :desc)
+    else
+      @allPosts = Post.all.order(created_at: :desc)
+    end
+  end  
 end
